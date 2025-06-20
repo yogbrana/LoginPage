f787f65e601cf1e3fdbf825c42f0112e0f5a199c
yogbrana <yograna08@gmail.com>
2025-06-21 00:41:38 +0545

    write logout.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
     <div class="container" id="signUp" style="display:none;" >
         <h1 class="form-title">Register</h1>
-        <form method="post" action="">
+        <form method="post" action="register.php">
             <div class="input-group">
                 <i class="fas fa-user"></i>
                 <input type="text" name="fName" id="fName" placeholder="First Name" required>
@@ -48,7 +48,7 @@
 
     <div class="container" id="signIn"  >
         <h1 class="form-title">Sign In</h1>
-        <form method="post" action="">
+        <form method="post" action="register.php">
             
             <div class="input-group">
                 <i class="fas fa-envelope"></i>
